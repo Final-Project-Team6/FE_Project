25f0488c831eaba4b15547fe800677154e3701a2
subbi <92204014+subinsad@users.noreply.github.com>
2024-05-07 11:34:48 +0900

    🎉INIT: (Begin a project) 프로젝트 시작

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,11 @@
+'use client';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 30px;
+  color: #111;
+`;
+
+export default function Header() {
+  return <Title> hi</Title>;
+}
