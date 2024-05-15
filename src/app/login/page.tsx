f7f2247f907246cba487057e7e4749446182043b
subbi <92204014+subinsad@users.noreply.github.com>
2024-05-16 01:30:25 +0900

    💄UXUI: login페이지 퍼블리싱

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,55 @@
+import './login.scss'
+
+import Link from 'next/link'
 import React from 'react'
+
+import CheckBox from '@/components/checkBox/CheckBox'
+import Button from '@/components/common/Button'
+import Input from '@/components/common/Input/Input'
+import { joinPlaceholder, loginPlaceholder } from '@/constants/inputPlaceholder'
 
 function page() {
   return (
-    <div>
-      <h1>로그인</h1>
-      <div />
+    <div className="login-wrap">
+      <h1 className="title">로그인</h1>
+      <div className="input-box">
+        <div className="input-box-item">
+          <Input
+            id="id"
+            type="id"
+            required
+            placeholder={joinPlaceholder.id}
+          />
+
+          <Input
+            id="password1"
+            type="password"
+            required
+            placeholder={loginPlaceholder.password}
+            passwordIcon
+          />
+        </div>
+
+        <CheckBox big>자동로그인</CheckBox>
+
+        <p className="input-box-error body_06">
+          아이디 또는 비밀번호가 올바르지 않습니다. 다시 확인해주세요
+        </p>
+      </div>
+
+      <div className="btn-box">
+        <Button
+          size="confirm"
+          color="primary">
+          로그인
+        </Button>
+        <div className="btn-box-serch body_05">
+          <Link href="/"> 아이디 찾기</Link>
+          <Link href="/"> 비밀번호 찾기</Link>
+        </div>
+
+        <Button size="confirm">회원가입</Button>
+      </div>
     </div>
   )
 }
