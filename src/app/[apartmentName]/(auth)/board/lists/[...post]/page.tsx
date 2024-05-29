4f6289be2504c1420f74889b353b349f67a890ff
dlwnghd <alion0513@naver.com>
2024-05-29 18:24:32 +0900

    💄UXUI: tab 추가

--- src/app/[apartmentName]/(auth)/board/lists/[...post]/page.tsx
+++ src/app/[apartmentName]/(auth)/board/lists/[...post]/page.tsx
@@ -1,5 +1,7 @@
 import SearchBar from '@/components/searchBar/SearchBar'
+import Tap from '@/components/tap/Tap'
 import { dropDown } from '@/constants/dropDown'
+import { tapList } from '@/constants/tapList'
 
 export default function Page({ params }: { params: { post: string } }) {
   return (
@@ -14,6 +16,7 @@ export default function Page({ params }: { params: { post: string } }) {
         id="test2"
         placeholder="검색어를 입력해주세요"
       />
+      <Tap tapList={tapList} />
     </div>
   )
 }
