55890421a0d9e0f94c206bcef40dd38574215573
dlwnghd <alion0513@naver.com>
2024-06-13 10:00:45 +0900

    ♻️REF : 글작성 시 에디터 비활성화 버그 수정

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,38 +1,50 @@
 'use client'
-import Script from 'next/script'
+import { useCallback, useEffect } from 'react'
+
+declare global {
+  interface Window {
+    $: any
+  }
+}
 
 export default function Editor() {
-  return (
-    <>
-      <div id="summernote" />
-      <Script
-        id="show-editor"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            $('#summernote').summernote({
-              height: 450,
-              lang: 'ko-KR',
-              disableDragAndDrop: true,
-              disableResizeEditor: true,
-              tabDisable: false,
-              placeholder: '내용을 입력하세요',
-              toolbar: [
-                ['insert', ['hr']],
-                ['style', ['style']],
-                ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
-                ['fontsize', ['fontsize']],
-                ['fontname', ['fontname']],
-                ['color', ['color', 'backcolor']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['height', ['height']],
-                ['table', ['table']],
-                ['view', ['help']]
-              ]
-            });
-          `,
-        }}
-      />
-    </>
-  )
+  const initSummernote = useCallback(() => {
+    if (typeof window !== 'undefined' && window.$) {
+      window.$('#summernote').summernote({
+        height: 450,
+        lang: 'ko-KR',
+        disableDragAndDrop: true,
+        disableResizeEditor: true,
+        tabDisable: false,
+        placeholder: '내용을 입력하세요',
+        icons: {
+          picture: 'postWriteImage',
+        },
+        toolbar: [
+          ['style', ['style']],
+          ['font', ['bold', 'underline', 'clear']],
+          ['fontsize', ['fontsize']],
+          ['fontname', ['fontname']],
+          ['color', ['color']],
+          ['para', ['ul', 'ol', 'paragraph']],
+          ['height', ['height']],
+          ['table', ['table']],
+          ['view', ['help']],
+        ],
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (typeof window !== 'undefined' && window.$) {
+        initSummernote()
+        clearInterval(interval)
+      }
+    }, 100)
+
+    return () => clearInterval(interval)
+  }, [initSummernote])
+
+  return <div id="summernote" />
 }
